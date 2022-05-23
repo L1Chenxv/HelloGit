ef176d29d0194be2e26ab382ce37054a25ec3bee
L1Chenxv <943429931@qq.com>
2022-05-23 22:23:01 +0800

    更新7

--- HelloGit/src/main/java/com/lcx/git/HelloGit.java
+++ HelloGit/src/main/java/com/lcx/git/HelloGit.java
@@ -15,6 +15,7 @@ public class HelloGit {
         System.out.println("更新5!");
         System.out.println("更新6!");
         System.out.println("更新7!");
+        System.out.println("更新8!");
 
 
     }
