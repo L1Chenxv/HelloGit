475fbb2288615fc876c22e9704a66047a6b393e2
L1Chenxv <943429931@qq.com>
2022-05-23 20:18:16 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/lcx/git/HelloGit.java
@@ -0,0 +1,12 @@
+package com.lcx.git;
+
+/**
+ * version 1.0
+ * create: 2022-05-23 19:48
+ * author: 李陈旭
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
